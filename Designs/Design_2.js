2f9a602d792969795932b0c4fce9d56c730e3e69
SeeleyX <william.seeley@hotmail.com>
2022-06-22 20:26:41 +0100

    A few designs that are offline not on django

---
+++ Designs/Design_2.js
@@ -0,0 +1,8 @@
+function showQuantum() {
+    var x = document.getElementById("media-scroller");
+    if (x.style.display === "none") {
+      x.style.display = "grid";
+    } else {
+      x.style.display = "none";
+    }
+  }

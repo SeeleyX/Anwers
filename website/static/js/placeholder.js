933867667adbdf6638573fa43470372962303dc1
SeeleyX <william.seeley@hotmail.com>
2022-06-27 16:54:23 +0100

    Physics Page

--- website/static/js/placeholder.js
+++ website/static/js/placeholder.js
@@ -1,24 +1,9 @@
 
-// $(document).ready(function(){
-//   $(".nav-bar-button").click(function(){
-//     $(".title-slide").toggleClass("a-button");
-//     $(".nav-bar-button").toggleClass("hover-button");
-//     $("rect").toggleClass("icon-button");
-//     $("circle").toggleClass("icon-button");
-//     $("path").toggleClass("icon-button");
-//   });
-// });
 
-$(document).ready(function () {
-
-    //this will attach the class to every target 
-    $(document).on('click', function (event) {
-        $target = $(event.target);   
-           $(".title-slide").toggleClass("a-button");
-           $target.toggleClass("hover-button");
-           $("rect").toggleClass("icon-button");
-           $("circle").toggleClass("icon-button");
-           $("path").toggleClass("icon-button");
-       });
-
-   })
+$(document).ready(function(){
+  $(".nav-bar-button").click(function(){
+    $(".title-slide").toggleClass("a-button");
+    $(".nav-bar-button").toggleClass("hover-button");
+    $(".logo").toggleClass("i-button");
+  });
+});

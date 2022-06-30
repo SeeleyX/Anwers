a9858e0a02f66727c07e8a92ba7a73084be792d4
SeeleyX <william.seeley@hotmail.com>
2022-06-30 13:16:05 +0100

    taken nav bar out of login

--- website/learn/urls.py
+++ website/learn/urls.py
@@ -6,7 +6,7 @@ app_name = 'learn'
 
 urlpatterns = [
 	path('', views.learn_base, name="learn_base"),
-	path('index', views.learn_base, name='index')
+	path('index', views.learn_base, name='index'),
 	path('astronomy/', views.astronomy, name='astronomy'),
 	path('login/', views.user_login, name='user_login'),
 	path('register/', views.user_register, name='user_register'),

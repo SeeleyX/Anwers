65baabaeee8df18903927a2f045c837eb434e833
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-07-28 18:56:20 +0100

    Sam: Fixed JS for base navbar hover

--- website/static/js/placeholder.js
+++ website/static/js/placeholder.js
@@ -1,9 +1,25 @@
 
-
 $(document).ready(function(){
+  $().click(function(event) {
+    let $target = $(event.target);
+    if (!$target.closest('.nav-bar-button')) {
+      $('.nav-bar-button').children("i").removeClass("i-button");
+    }
+  })
+
   $(".nav-bar-button").click(function(){
-    $(".title-slide").toggleClass("a-button");
-    $(".nav-bar-button").toggleClass("hover-button");
-    $(".logo").toggleClass("i-button");
+    let iChild = $(this).children("i");
+    let aChild = $(this).children("a");
+    iChild.addClass("i-button");
+    aChild.addClass("a-button");
+    $(this).addClass("hover-button");
+
   });
+  $(".nav-bar-button").mouseleave(function() {
+    let iChild = $(this).children("i");
+    let aChild = $(this).children("a");
+    aChild.removeClass("a-button");
+    iChild.removeClass("i-button");
+    $(this).removeClass("hover-button")
+  })
 });
